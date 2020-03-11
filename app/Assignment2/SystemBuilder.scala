d269028b5256cc4d11e8c7e6868a6ada7b1e4ab1
Karuna Puri <kpn6677@MLLAINPUAN1502-0018.local>
2020-03-11 14:43:30 +0530

    Add error condition Abort for Ass.2

--- app/Assignment2/SystemBuilder.scala
+++ app/Assignment2/SystemBuilder.scala
@@ -1,24 +1,43 @@
 package Assignment2
 
 import Assignment2.helpers._
+import Assignment2.models._
 
-object SystemBuilder {
-  def main(args: Array[String]) = {
+case class Widget(isRectangleWidgetBuild: Unit,
+                  isSquareWidget:SquareWidget,
+                  isEllipseWidget:EllipseWidget,
+                  isCircleWidget: CircleWidget,
+                  isTextBoxWidget: TextBoxWidget)
+
+object SystemBuilder extends  App{
     println("----------------------------------------------------------------")
     println("Bill of Materials")
     println("----------------------------------------------------------------")
 
-    //Builder pattern using Abstract Class
+  /*
+   * Rectangle Widget Builder Pattern is designed using Abstract Class
+   * (which is similar to other programming languages like Java)
+   * and rest all widgets (like Square, Circle, Ellipse, and TextBox)
+   * are designed using Scala case class concept. Reason for doing so is
+   * to demonstrate possible designs for designing Builder Design Pattern in Scala
+   */
+
     val rectangleWidgetBuild = new RectangleWidgetBuild().withPositionX(10).withPositionY(10).withWidth(30).withHeight(40)
     val rectangleWidget = rectangleWidgetBuild.build
-    println("Rectangle " + rectangleWidget)
+    val rectangleWidgetFormat = println("Rectangle " + rectangleWidget)
 
-    //Builder pattern using Scala case class
-    SquareWidgetBuild.defaultValuesBuild
-    EllipseWidgetBuild.defaultValuesBuild
-    CircleWidgetBuild.defaultValuesBuild
-    TextBoxWidgetBuild.defaultValuesBuild
+    Widget match {
+
+        case _: Widget.type =>
+          Widget(
+          isRectangleWidgetBuild = rectangleWidgetFormat,
+          isSquareWidget = SquareWidgetBuild.defaultValuesBuild,
+          isEllipseWidget = EllipseWidgetBuild.defaultValuesBuild,
+          isCircleWidget = CircleWidgetBuild.defaultValuesBuild,
+          isTextBoxWidget = TextBoxWidgetBuild.defaultValuesBuild
+          )
+        case _ => println ("+++++Abort+++++")
+      }
 
     println("----------------------------------------------------------------")
   }
-}
